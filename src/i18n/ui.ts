d513df5591e16dad1b614801a966e0962e7aa205
Antonio Garosi <antonio_garosi@yahoo.it>
2025-11-02 22:37:32 +0100

    Create i18n utilities

---
+++ src/i18n/ui.ts
@@ -0,0 +1,28 @@
+export const languages = {
+  en: 'English',
+  it: 'Italiano',
+};
+
+export const defaultLang = 'it';
+
+export const ui = {
+  it: {
+    'intro.title': 'il progetto',
+    'intro.description': 'Siamo una Start-Up innovativa specializzati in biodiversità e servizi di impollinazione, nata dalla collaborazione di Università e centri di ricerca di settore, Tramite monitoraggio sul campo, impollinazione di precisione, e divulgazione scientifica analizziamo i dati raccolti per dare supporto agli agricoltori e valutare lo stato di biodiversità di un habitat.',
+    'bi.id1.title': '',
+    'bi.id1.description': '',
+    'bi.id2.title': '',
+    'bi.id2.description': '',
+    'bi.id3.title': '',
+    'bi.id3.description': '',
+    'cta.claim': '',
+    'cta.button': '',
+    'footer.address': '',
+    'footer.cf': '',
+    'footer.vat': ''
+  },
+  en: {
+    'intro.title': 'the project',
+    'intro.description': 'We are an innovative Start-Up specialized in biodiversity and pollination services, born from the collaboration of Universities and sector research centers. Through field monitoring, precision pollination, and scientific dissemination, we analyze the collected data to support farmers and assess the biodiversity status of a habitat.',
+  }
+} as const;
